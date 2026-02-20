67ac7ad0247b97ba7f2f4ddc906ad45f982c3bb0
D-Galahad <dml.daniel6@gmail.com>
2026-02-20 08:47:04 +0100

    first commit

---
+++ danielml/src/main/java/com/examenrico/danielml/DanielmlApplication.java
@@ -0,0 +1,13 @@
+package com.examenrico.danielml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanielmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanielmlApplication.class, args);
+	}
+
+}
---
+++ danielml/src/test/java/com/examenrico/danielml/DanielmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examenrico.danielml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanielmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
